0fec2a3ccfb3be6d23961bafe6821e29dd900e0d
Inderdeep Singh <inderdeep.singh@laxaar.com>
2019-04-20 16:36:52 +0530

    Dependent dropdowns

--- src/app/auction/create-auction/create-auction.component.ts
+++ src/app/auction/create-auction/create-auction.component.ts
@@ -8,6 +8,7 @@ import {UnitService} from '../../services/unit.service';
 import {UserService} from '../../services/user.service';
 import {UsersellerService} from '../../services/seller.service';
 import {BuyerService} from '../../services/buyer.service';
+import {ListingService} from '../../services/listing.service';
 
 
 @Component({
@@ -19,22 +20,50 @@ export class CreateAuctionComponent implements OnInit {
 
   form = new FormGroup({
     newItem: new FormGroup({
-      itemName: new FormControl(''),
-      itemCategory: new FormControl(''),
-      sampleNo: new FormControl(''),
-      auctionType: new FormControl('seller'),
-      availableQty: new FormControl,
-      minQty: new FormControl,
-      maxQty: new FormControl,
-      unit: new FormControl(''),
-      floorPrice: new FormControl,
-      ceilingPrice: new FormControl,
-      nameVisible: new FormControl(1),
-      startTime: new FormControl,
-      endTime: new FormControl,
+      itemName: new FormControl('', [
+        Validators.required,
+      ]),
+      itemCategory: new FormControl('', [
+        Validators.required,
+      ]),
+      sampleNo: new FormControl('', [
+        Validators.required,
+      ]),
+      auctionType: new FormControl('seller', [
+        Validators.required,
+      ]),
+      availableQty: new FormControl('', [
+        Validators.required,
+      ]),
+      minQty: new FormControl('', [
+        Validators.required,
+      ]),
+      maxQty: new FormControl('', [
+        Validators.required,
+      ]),
+      unit: new FormControl('', [
+        Validators.required
+      ]),
+      floorPrice: new FormControl('', [
+        Validators.required,
+      ]),
+      ceilingPrice: new FormControl('', [
+        Validators.required,
+      ]),
+      nameVisible: new FormControl(1, [
+        Validators.required,
+      ]),
+      startTime: new FormControl(new Date(), [
+        Validators.required,
+      ]),
+      endTime: new FormControl(new Date(), [
+        Validators.required,
+      ]),
       seller: new FormControl(''),
       buyer: new FormControl(''),
-      transportCost: new FormControl(1)
+      transportCost: new FormControl(1, [
+        Validators.required,
+      ])
     })
   });
   itemnames: any;
@@ -42,29 +71,57 @@ export class CreateAuctionComponent implements OnInit {
   units: any;
   sellers: any;
   buyers: any;
+  listings: any;
 
   constructor(private categoryService: CategoryService,
               private itemnameService: ItemnameService,
               private unitService: UnitService,
               private sellerService: UsersellerService,
               private buyerService: BuyerService,
+              private listingService: ListingService,
               private router: Router
   ) {
 
   }
 
   ngOnInit() {
+    console.log(this.form.value.newItem)
+    forkJoin([this.itemnameService.getAll(), this.unitService.getAll(), this.sellerService.getAll(), this.buyerService.getAll(),
 
-    forkJoin([this.itemnameService.getAll(), this.categoryService.getAll(), this.unitService.getAll(), this.sellerService.getAll(), this.buyerService.getAll()])
+    ])
       .subscribe(response => {
-        this.categories = response[0];
-        this.itemnames = response[1];
-        this.units = response[2];
-        this.sellers = response[3];
-        this.buyers = response[4];
+        this.itemnames = response[0];
+        this.units = response[1];
+        this.sellers = response[2];
+        this.buyers = response[3];
       }, (error: Response) => {
         console.log(error);
       });
   }
 
+  onItemChange() {
+    let item = this.form.get('newItem.itemName').value;
+    this.categories = [];
+    this.listings = [];
+    this.categoryService.getCategoriesByItem(item).subscribe((response) => {
+      this.categories = response;
+    }, (error: Response) => {
+      console.log(error);
+    });
+
+  }
+
+  onCategoryChange() {
+    this.listings = [];
+    let category = this.form.get('newItem.itemCategory').value;
+    this.listingService.getListingsByCategory(category).subscribe((response) => {
+      this.listings = response;
+    }, (error: Response) => {
+      console.log(error);
+    });
+  }
+
+  save(event) {
+    event.preventDefault();
+  }
 }
